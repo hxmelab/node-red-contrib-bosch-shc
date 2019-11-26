e43f64209c93c3b4cb28d19e759627df69db5e2d
hxmelab <phil.net@gmx.net>
2019-11-27 00:13:58 +0100

    Add put state

--- nodes/shc-device.js
+++ nodes/shc-device.js
@@ -7,6 +7,7 @@ module.exports = function(RED) {
             RED.nodes.createNode(this, config);
 
             this.deviceId = config.device.split('|')[1];
+            this.deviceModel = config.device.split('|')[2];
             this.serviceId = config.service;
             this.state = config.state;
             this.name = config.name;
@@ -17,13 +18,22 @@ module.exports = function(RED) {
              * 
              */
             this.on('input', function(msg, send, done) {
-                if (this.shcConfig && this.shcConfig.state === 'PAIRED') {                    
-                    this.shcConfig.shc.getBshcClient().getDeviceServices(this.deviceId, this.serviceId).subscribe(result => {
-                        send(this.setMsgObject(result, msg));
-                        done();
-                    }, err => {
-                        done(err);
-                    });
+                if (this.shcConfig && this.shcConfig.state === 'PAIRED') {
+                    if (this.isValid(msg.payload) && this.getServiceBody(msg.payload)) {
+                        this.shcConfig.shc.getBshcClient().putState(this.getPath(), this.getServiceBody(msg.payload)).subscribe(result => {
+                            send(result);
+                            done();
+                        }, err => {
+                            done(err);
+                        });
+                    } else {
+                        this.shcConfig.shc.getBshcClient().getDeviceServices(this.deviceId, this.serviceId).subscribe(result => {
+                            send(this.setMsgObject(result, msg));
+                            done();
+                        }, err => {
+                            done(err);
+                        });
+                    }
                 }
             });        
             
@@ -66,6 +76,39 @@ module.exports = function(RED) {
                     (this.deviceId === '*' && this.serviceId === msg.id) ||
                     (this.deviceId === msg.deviceId && this.serviceId === '*') ||
                     (this.deviceId === msg.deviceId && this.serviceId === msg.id));
+        }
+
+        isValid(newState) {
+            switch(this.deviceModel) {
+                case 'SD':
+                case 'BSM':
+                case 'PSM':
+                case 'CAMERA_EYES':
+                case 'CAMERA_360':
+                case 'TWINGUARD':
+                case 'INTRUSION_DETECTION_SYSTEM':
+                case 'PRESENCE_SIMULATION_SERVICE': return (typeof newState === 'boolean');
+                case 'BBL': return (typeof newState === 'number' && newState >= 0.00 && newState <= 1.00);
+                case 'ROOM_CLIMATE_CONTROL': return (typeof newState === 'number' && newState > 4 && newState < 31);
+                default: return false;
+            }
+        }
+
+        getPath() {
+            return 'devices/' + this.deviceId + '/services/' + this.serviceId;
+        }
+
+        getServiceBody(newState) {
+            switch(this.serviceId) {
+                case 'SmokeDetectorCheck': return {'@type': 'smokeDetectorCheckState', 'value': 'SMOKE_TEST_REQUESTED'};
+                case 'PowerSwitch': return {'@type': 'powerSwitchState', 'switchState': (newState ? 'ON' : 'OFF')};
+                case 'RoomClimateControl': return {'@type': 'climateControlState', 'setpointTemperature': newState};
+                case 'PrivacyMode': return {'@type': 'privacyModeState', 'value': (newState ? 'ENABLED':'DISABLED')};
+                case 'IntrusionDetectionControl': return {'@type': 'intrusionDetectionControlState', 'value': ( newState ? 'SYSTEM_ARMED':'SYSTEM_DISARMED' )};
+                case 'PresenceSimulationConfiguration': return {'@type': 'presenceSimulationConfigurationState', 'enabled': newState};
+                case 'ShutterControl': return {'@type': 'shutterControlState', 'level': newState};
+                default: return false;
+            }
         }
 
         shcEventsHandler = (data) => {
