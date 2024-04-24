b2fdd55edf83dc8c6214596240546543bea24a01
saemmy <saemmy@gmx.net>
2024-04-24 17:08:27 +0200

    Remove unsued code for openDoors

--- nodes/shc-opendoors.js
+++ nodes/shc-opendoors.js
@@ -3,8 +3,6 @@ module.exports = function (RED) {
     class SHCOpenDoorsNode {
         constructor(config) {
             RED.nodes.createNode(this, config);
-            this.stateName = config.state.split('|')[0];
-            this.stateId = config.state.split('|')[1];
             this.name = config.name;
             this.shcConfig = RED.nodes.getNode(config.shc);
             if (this.shcConfig) {
@@ -12,7 +10,7 @@ module.exports = function (RED) {
                 this.shcConfig.registerListener(this);
             }
             /**
-             * Enable/disable user defined state if payload is boolean, otherwise get the state
+             * get open doors or windows
              */
             this.on('input', (msg, send, done) => {
                 if (this.stateId && this.shcConfig && this.shcConfig.connected) {
@@ -26,17 +24,9 @@ module.exports = function (RED) {
             });
         }
         setMsgObject(data) {
-            const msg = {topic: (this.name || this.stateName)};
+            const msg = {topic: (this.name )};
             msg.payload = data;
             return msg.payload === null ? null : msg;
-        }
-        listener(data) {
-            const parsed = JSON.parse(JSON.stringify(data));
-            parsed.forEach(event => {
-                if (event.id && this.stateId === event.id) {
-                    this.send(this.setMsgObject(event));
-                }
-            });
         }
     }
     RED.nodes.registerType('shc-opendoors', SHCOpenDoorsNode);
