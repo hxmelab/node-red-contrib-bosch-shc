122720cb3482ba8ebe463e02de3c610fcfb06e62
saemmy <saemmy@gmx.net>
2024-05-12 19:31:36 +0200

    Add new nodes for OpenDoorsWindows and for UserDefinedStates (#78)

---
+++ nodes/shc-opendoors.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = function (RED) {
+    class SHCOpendoorsNode {
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+            this.name = config.name;
+            this.shcConfig = RED.nodes.getNode(config.shc);
+            if (this.shcConfig) {
+                this.shcConfig.checkConnection(this);
+                this.shcConfig.registerListener(this);
+            }
+            /**
+             * Get Opendoors state
+             */
+            this.on('input', (msg, send, done) => {
+                if (this.shcConfig && this.shcConfig.connected) {
+                    this.shcConfig.shc.getBshcClient()
+                        .getOpenWindows().subscribe(result => {
+                            send(this.setMsgObject(result._parsedResponse));
+                            done();
+                        }, err => {
+                            done(err);
+                        });
+                }
+            });
+        }
+        setMsgObject(data) {
+            const msg = {topic: (this.name )};
+            msg.payload = data;
+            return msg.payload === null ? null : msg;
+        }
+        listener(data) {
+            // do nothing, no events for open-doors-windows
+        }
+
+    }
+    RED.nodes.registerType('shc-opendoors', SHCOpendoorsNode);
+};
